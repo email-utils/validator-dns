1a1483aa78fd84a962f433662dd840f6c9cb5559
Simeon Smith <simeon.smith@14four.com>
2021-12-01 14:17:46 -0800

    Cleanup.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,34 +1,3 @@
-interface EmailValidatorConfig {
-  local: LocalValidateConfig
-  domain: DomainValidateConfig
-}
-interface EmailValidatorParam {
-  local?: LocalValidateConfig
-  domain?: DomainValidateConfig
-}
-type LocalValidateConfig = {
-  alphaUpper: boolean
-  alphaLower: boolean
-  numeric: boolean
-  period: boolean
-  printable: boolean
-  quote: boolean
-  hyphen: boolean
-  spaces: boolean
-}
-
-type DomainValidateConfig = {
-  alphaUpper: boolean
-  alphaLower: boolean
-  numeric: boolean
-  period: boolean
-  hyphen: boolean
-  tld: boolean
-  localhost: boolean
-  charsBeforeDot: number // -1 means don't check.
-  charsAfterDot: number // -1 means don't check.
-}
-
 type DnsConfig = {
   a: number // -1 means don't check.
   ns: number // -1 means don't check.
@@ -47,23 +16,4 @@ type DnsParam = {
   mx: number // -1 means don't check.
   validScore: number // -1 means don't check.
   smtpPorts: number[]
-}
-
-type LocalSanitizeConfig = {
-  removePeriods: boolean
-  removePlusTag: boolean
-}
-
-type CommonSanitizeConfig = {
-  lowercase: boolean
-}
-
-type SanitizeConfig = {
-  common: CommonSanitizeConfig
-  local: LocalSanitizeConfig
-}
-
-type SanitizeParam = {
-  common?: CommonSanitizeConfig
-  local?: LocalSanitizeConfig
 }
---
+++ bin/tag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $1 ]; then
+  echo "Creating a new $1 version."
+
+  npm version $1
+  version=`node -e "console.log(require('./package.json').version);"`
+
+  npm run build
+  git add -A
+  git commit -m "Build for $version."
+
+  echo "Publishing version $version."
+
+  git push
+  git push --tags
+  npm publish --access public
+else
+  echo "No version type provided. (ie. ./bin/tag.sh version)";
+fi
+
